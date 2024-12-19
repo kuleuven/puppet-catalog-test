22c01aa295c2608096da562985c015b59f903a86
Jean-François Roche <jfroche@affinitic.be>
2024-12-19 16:52:33 +0100

    Fix Ruby 2 support
    `except` is available from Ruby 3

--- lib/puppet-catalog-test/puppet_adapter/puppet_4x_adapter.rb
+++ lib/puppet-catalog-test/puppet_adapter/puppet_4x_adapter.rb
@@ -32,7 +32,11 @@ class PuppetCatalogTest::Puppet4xAdapter < PuppetCatalogTest::BasePuppetAdapter
     Puppet::Test::TestHelper.before_each_test
     init_config
     node = Puppet::Node.new(hostname, facts: Puppet::Node::Facts.new('facts', facts))
-    node.merge(facts.except('trusted'))
+    if RUBY_VERSION >= '3.0'
+      node.merge(facts.except('trusted'))
+    else
+      node.merge(facts.reject { |k, _v| k == 'trusted' })
+    end
     node.trusted_data = facts['trusted']
     node
   end
